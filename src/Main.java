d873c27b3883ec0ae46e823b040e9a366e15a3b0
UrbancokDavid <urbancok.david@outlook.com>
2019-03-05 19:51:09 +0100

    Add code from first three lectures

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //FirstMain.firstMain(null);
+
+        //SecondMain.secondMain(null);
+
+        ThirdMain.thirdMain(null);
+    }
+}
